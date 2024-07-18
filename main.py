00abc96d3810fb8f0d0cda1cb3a21e8cada94038
rayhanajiee <90242134+rayhanajiee@users.noreply.github.com>
2024-07-18 15:35:13 +0700

    Add files via upload

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from website import create_app
+from gevent.pywsgi import WSGIServer
 
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host = "0.0.0.0", port = 5000)
+        # Run the app with Gevent WSGI server
